a494fbbae1313c74d646920838fda56980d5407a
Jarod Wilson <jaroddwilson@gmail.com>
2022-03-29 22:05:34 -0400

    Sold Out functionality implemented

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
+
+  const [inStock, setInStock] = useState(true)
+
+  function handleStock() {
+    setInStock(!inStock)
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleStock}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStock}>Sold Out</button>
       )}
     </li>
   );
